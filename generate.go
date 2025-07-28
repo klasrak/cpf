fd27d293b6a8f5c5c7a5810cf2d3efe4d217e3f5
Danilo Augusto <dasfcm@gmail.com>
2025-07-28 19:53:04 -0300

    feat: add digits for each brazilian state

---
+++ generate.go
@@ -0,0 +1,18 @@
+package cpf
+
+var digits = map[string]int{
+	"RS": 0,
+	"DF": 1, "GO": 1, "MS": 1, "MT": 1, "TO": 1,
+	"AC": 2, "AM": 2, "AP": 2, "PA": 2, "RO": 2, "RR": 2,
+	"CE": 3, "MA": 3, "PI": 3,
+	"AL": 4, "PB": 4, "PE": 4, "RN": 4,
+	"BA": 5, "SE": 5,
+	"MG": 6,
+	"ES": 7, "RJ": 7,
+	"SP": 8,
+	"PR": 9, "SC": 9,
+}
+
+func generate(state string) string {
+	return "123.456.789-00" // Placeholder for actual CPF generation logic
+}
